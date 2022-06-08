ae89c940fcee02263daeb0c61c73beeb4bac1c61
dpalme <diegoPalmerin24@gmail.com>
2022-06-08 15:23:58 -0500

    local frontend finished

--- front/router.rb
+++ front/router.rb
@@ -2,7 +2,7 @@ require "sinatra"
 require "json"
 require "faraday"
 
-LEADERBOARD_GW = ""
+LEADERBOARD_GW = "https://7vgoqpvl66.execute-api.us-east-1.amazonaws.com/default/leaderboard"
 QUESTIONS_GW = "https://atw3z54ud4.execute-api.us-east-1.amazonaws.com/default/questions"
 
 get "/" do
@@ -13,24 +13,81 @@ get "/leaderboard" do
   response = Faraday.get(LEADERBOARD_GW + "/scores")
   @data = []
   if response.success?
-    @data = JSON.parse(response.body)
+    @data = JSON.parse response.body
   end
   erb :leaderboard
 end
 
+post "/leaderboard" do
+  conn = Faraday.new(
+    url: LEADERBOARD_GW,
+    params: { param: "1" },
+    headers: { "Content-Type" => "application/json" },
+  )
+
+  response = conn.post("/") do |req|
+    req.body = { username: params[:username],
+                 score: params[:score],
+                 num_of_questions: params[:num_of_questions] }.to_json
+  end
+
+  erb :leaderboard
+end
+
 get "/quiz" do
-  n = params[:questions]
-  if n.to_i < 1 || n.to_i > 10
+  if !params.key? :questions
+    return erb :index
+  end
+  begin
+    @n = params[:questions].to_i
+  rescue Exception
     @errorMessage = "Question number must be between 1 and 10"
     return erb :error
   end
-  response = Faraday.get(QUESTIONS_GW)
-  @totalQuestions = n
-  @data = []
-  if response.success?
-    @data = JSON.parse(response.body).map do |entry|
-      JSON.parse(entry)
+  if @n.to_i < 1 || @n.to_i > 10
+    @errorMessage = "Question number must be between 1 and 10"
+    return erb :error
+  end
+  response = Faraday.get(QUESTIONS_GW, { n: @n })
+  @data = JSON.parse [
+                       {
+                         "Question": "¿Tuleperaconlapapaya?",
+                         Answers: [
+                           { Text: "Sip", Correct: true },
+                           { Text: "Nop", Correct: false },
+                           { Text: "Tampoco", Correct: false },
+                         ],
+                       },
+                       {
+                         "Question": "¿Tuleperaconlapapaya2?",
+                         Answers: [
+                           { Text: "Sip", Correct: true },
+                           { Text: "Nop", Correct: false },
+                           { Text: "Tampoco", Correct: false },
+                         ],
+                       },
+                     ].to_json
+  # if response.success?
+  #   @data = JSON.parse(response.body).map do |entry|
+  #     JSON.parse(entry)
+  #   end
+  # end
+  @n = @data.length
+  @data.slice(0, 10)
+  erb :questions
+end
+
+post "/quiz" do
+  @questions = request.body
+  p @questions.string
+  @score = 0
+  @numQuestions = 0
+  @questions.each "&" do |k|
+    p k
+    @numQuestions += 1
+    if k[2] == "t"
+      @score += 1
     end
   end
-  erb :question
+  erb :quizScore
 end
