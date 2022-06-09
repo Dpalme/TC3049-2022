544986328b738f190f9d94858847139e0d665d08
Fernando Melgar <fernando.melgar@qacg.com>
2022-06-09 12:35:18 -0500

    Question by question

--- front/router.rb
+++ front/router.rb
@@ -1,31 +1,31 @@
-require "sinatra"
-require "json"
-require "faraday"
+require 'sinatra'
+require 'json'
+require 'faraday'
 
-LEADERBOARD_GW = "https://7vgoqpvl66.execute-api.us-east-1.amazonaws.com/default/leaderboard"
-QUESTIONS_GW = "https://atw3z54ud4.execute-api.us-east-1.amazonaws.com/default/questions"
+LEADERBOARD_GW = 'https://7vgoqpvl66.execute-api.us-east-1.amazonaws.com/default/leaderboard'
+QUESTIONS_GW = 'https://atw3z54ud4.execute-api.us-east-1.amazonaws.com/default/questions'
 
-get "/" do
+set :sessions, true
+
+get '/' do
   erb :index
 end
 
-get "/leaderboard" do
-  response = Faraday.get(LEADERBOARD_GW + "/scores")
+get '/leaderboard' do
+  response = Faraday.get("#{LEADERBOARD_GW}/scores")
   @data = []
-  if response.success?
-    @data = JSON.parse response.body
-  end
+  @data = JSON.parse response.body if response.success?
   erb :leaderboard
 end
 
-post "/leaderboard" do
+post '/leaderboard' do
   conn = Faraday.new(
     url: LEADERBOARD_GW,
-    params: { param: "1" },
-    headers: { "Content-Type" => "application/json" },
+    params: { param: '1' },
+    headers: { 'Content-Type' => 'application/json' },
   )
 
-  response = conn.post("/") do |req|
+  response = conn.post('/') do |req|
     req.body = { username: params[:username],
                  score: params[:score],
                  num_of_questions: params[:num_of_questions] }.to_json
@@ -34,8 +34,56 @@ post "/leaderboard" do
   erb :leaderboard
 end
 
+get '/quiz' do
+  session[:questions] = fetch_questions # TODO: Aquí hacer el fetch correcto de datos.
+  session[:total] = params[:total].nil? ? 10 : params[:total].to_i
+  session[:score] = 0
+  erb redirect '/quiz/inProgress?current=0'
+end
+
+get '/quiz/inProgress' do
+  @current_question = params['current'].to_i
+  @n = session[:total]
+  @data = [session[:questions][@current_question]] # Lo meto en un arreglo para que siga funcionando con la lógica de antes
+  erb :questions
+end
+
+post '/quiz' do
+  session[:score] += 1
+  next_question = params['currentQuestion'].to_i + 1
+  if next_question < session[:total]
+    redirect "/quiz/inProgress?current=#{next_question}"
+  else
+    # Compute and save score
+    'You won'
+  end
+end
+
+def fetch_questions
+  JSON.parse [
+               {
+                 "Question": "¿Tuleperaconlapapaya?",
+                 Answers: [
+                   { Text: "Sip", Correct: true },
+                   { Text: "Nop", Correct: false },
+                   { Text: "Tampoco", Correct: false }
+                 ]
+               },
+               {
+                 "Question": "¿Tuleperaconlapapaya2?",
+                 Answers: [
+                   { Text: "Sip", Correct: true },
+                   { Text: "Nop", Correct: false },
+                   { Text: "Tampoco", Correct: false }
+                 ]
+               }
+             ].to_json
+end
+
+=begin
 get "/quiz" do
-  if !params.key? :questions
+  current_question = params[:currentQuestion].to_i
+  @questions = current_question
     return erb :index
   end
   begin
@@ -50,23 +98,23 @@ get "/quiz" do
   end
   response = Faraday.get(QUESTIONS_GW, { n: @n })
   @data = JSON.parse [
-                       {
-                         "Question": "¿Tuleperaconlapapaya?",
-                         Answers: [
-                           { Text: "Sip", Correct: true },
-                           { Text: "Nop", Correct: false },
-                           { Text: "Tampoco", Correct: false },
-                         ],
-                       },
-                       {
-                         "Question": "¿Tuleperaconlapapaya2?",
-                         Answers: [
-                           { Text: "Sip", Correct: true },
-                           { Text: "Nop", Correct: false },
-                           { Text: "Tampoco", Correct: false },
-                         ],
-                       },
-                     ].to_json
+    {
+      "Question": "¿Tuleperaconlapapaya?",
+      Answers: [
+        { Text: "Sip", Correct: true },
+        { Text: "Nop", Correct: false },
+        { Text: "Tampoco", Correct: false },
+      ],
+    },
+    {
+      "Question": "¿Tuleperaconlapapaya2?",
+      Answers: [
+        { Text: "Sip", Correct: true },
+        { Text: "Nop", Correct: false },
+        { Text: "Tampoco", Correct: false },
+      ],
+    },
+  ].to_json
   # if response.success?
   #   @data = JSON.parse(response.body).map do |entry|
   #     JSON.parse(entry)
@@ -91,3 +139,4 @@ post "/quiz" do
   end
   erb :quizScore
 end
+=end
